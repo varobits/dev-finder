5c49403585d2b461155226bbe611841c1858b03a
varobits <a.aguerobonilla@gmail.com>
2022-08-03 11:11:31 +0200

    añadiendo estilos

--- src/main.js
+++ src/main.js
@@ -2,16 +2,16 @@ const searchFormBtn = document.querySelector('#searchBtn');
 const value = document.querySelector('#inputForm').value;
 searchFormBtn.addEventListener('click', getUser)
 
-
-const URL = "https://api.github.com/users/"
+const URL = "https://api.github.com/users/";
 
 async function getUser() {
+  const value = document.querySelector("#inputForm").value;
 
-    const value = document.querySelector('#inputForm').value;
-    
-    const res = await fetch (URL+value);
-    const data = await res.json();
+  const res = await fetch(URL + value);
+  const data = await res.json();
 
-    console.log(data)
+  console.log(data);
 
-}   
+  const userContent = document.querySelector("#userPreview");
+
+}
