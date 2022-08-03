44779967ae5caac7ae01959b3659c474ca34d3e1
varobits <a.aguerobonilla@gmail.com>
2022-08-03 15:00:37 +0200

    añadiendo nodos html

---
+++ src/node.js
@@ -0,0 +1,15 @@
+
+const userPreviewUser = document.querySelector("#userPreviewName");
+const userPreviewDate = document.querySelector("#userPreviewDate");
+const userPreviewBio = document.querySelector("#userPreviewBio");
+
+const userPreviewProjects = document.querySelector("#userPreviewProjects");
+const userPreviewRepos = document.querySelector("#userPreviewRepos");
+const userPreviewFollowers = document.querySelector("#userPreviewFollowers");
+const userPreviewFollowing = document.querySelector("#userPreviewFollowing");
+
+const userPreviewSocial = document.querySelector("#userPreviewSocial");
+const userPreviewSocial1 = document.querySelector("#userPreviewSocial1");
+const userPreviewSocial2 = document.querySelector("#userPreviewSocial2");
+const userPreviewSocial3 = document.querySelector("#userPreviewSocial3");
+const userPreviewSocial4 = document.querySelector("#userPreviewSocial4");
